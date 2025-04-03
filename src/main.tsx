fae50a7be581f0671281b395d4468b9b58374a93
Elvan Hidayat <elvanhd@gmail.com>
2025-04-03 10:19:46 +0700

    Refactor app structure and add activity management features
    Reorganized app layout with new components for improved modularity. Added functionality for creating, editing, and deleting activities, and introduced category-based media assets. Updated styles and integrated a new NavBar for better navigation.

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from './app/layout/App.tsx';
+import './app/layout/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
