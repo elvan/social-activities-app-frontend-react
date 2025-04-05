340ae0c726d888774e02f02a952398d72769e712
Elvan Hidayat <elvanhd@gmail.com>
2025-04-05 16:18:24 +0700

    Refactor activity details and enhance UI components
    Refactored the activity details into modular components for improved maintainability and readability. Added enhanced UI elements like filters, calendar, improved cards, and responsive layouts to enhance user experience. Introduced utility function for date formatting and updated dependencies to support new features.

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,13 +1,16 @@
 import { Grid2 } from '@mui/material';
+import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
 
 export default function ActivityDashboard() {
   return (
     <Grid2 container spacing={3}>
-      <Grid2 size={7}>
+      <Grid2 size={8}>
         <ActivityList />
       </Grid2>
-      <Grid2 size={5}>Activity filters go here</Grid2>
+      <Grid2 size={4}>
+        <ActivityFilters />
+      </Grid2>
     </Grid2>
   );
 }
