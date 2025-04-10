498b841102d99f7b983440481ec378dee66d547a
Elvan Hidayat <elvanhd@gmail.com>
2025-04-10 16:23:22 +0700

    Add activity hosting and attendance features with UI enhancements
    This update introduces logic for handling activity hosting, attendance, and cancellations with UI updates, including new reusable components (AvatarPopover, StyledButton, ProfileCard). Key functionality includes dynamic updates to activity-related UI, attendee listing, and integration with user roles (host/attendees).

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
+    https: true,
   },
-  plugins: [react()],
+  plugins: [mkcert(), react()],
 });
